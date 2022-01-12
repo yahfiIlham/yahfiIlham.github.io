d73e6e8df2b88611b983b0379151e1673b1e7b1f
Yahfi Ilham <yahfi.ilham@gmail.com>
2022-01-12 11:27:24 +0700

    initial init

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        console.log(window.scrollY === 0);
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Shrink the navbar 
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+});
